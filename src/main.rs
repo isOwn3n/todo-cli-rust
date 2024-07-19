9bfdf9aa158887ea6b639f81b7955de9aca9e4e3
ownen <mr.joker.mr.joke@gmail.com>
2024-07-20 03:23:28 +0330

    init database and clear screen

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,24 @@ fn main() -> std::io::Result<()> {
     sc.execute(Hide)?;
     enable_raw_mode()?;
 
+    // Init DB
+    let mut database = database::init_database().unwrap();
+
+    database::create_table(&mut database).unwrap();
+
     // Get todo data
-    let todos = database::get_data(maxl);
-    let mut todos_count = database::get_count().unwrap();
+    let todos = database::get_data(&mut database, maxl);
+    let mut todos_count = database::get_count(&mut database).unwrap();
 
     // Display data
-    let mut display = Display::new(
-        maxc,
-        maxl,
-        &mut (todos.len() as u16),
-        &mut todos_count,
-        todos.clone(),
-    );
+    let mut display = Display::new(maxc, maxl, &mut todos_count, todos.clone());
 
-    let _ = display::draw::draw(&mut display);
+    let _ = display::draw::draw(&mut display, display::components::main_components);
 
-    let _ = display.display_holder(&mut sc);
+    let _ = display.display_holder(&mut sc, &mut database);
 
     sc.execute(Show)?;
     disable_raw_mode()?;
+    clearscreen::clear().expect("");
     Ok(())
 }
