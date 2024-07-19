bb6fdf1dbcfce95a288f371972942e81d3622be3
ownen <mr.joker.mr.joke@gmail.com>
2024-07-20 03:22:56 +0330

    trying to create a function to have a textbox in terminal to user can create it todo task in terminal without using db manager

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,16 +1,25 @@
+use crossterm::{
+    cursor::{self, Hide, Show},
+    event::{self, Event, KeyCode, KeyEvent},
+    ExecutableCommand,
+};
+use draw::{draw, handle_pressed_keys};
 use std::{
     collections::HashMap,
-    io::{Stdout, Write},
+    io::{self, Stdout, Write},
+    time::Duration,
 };
 
-use draw::handle_pressed_keys;
+use crate::database::Database;
 
+pub mod components;
 pub mod draw;
 
 #[derive(Debug, PartialEq)]
 pub enum DisplayLocation {
     Main,
-    // Todo,
+    Todo,
+    Add,
     Quit,
 }
 
@@ -19,16 +28,19 @@ pub struct Display {
     maxl: u16,
     current_todo: u16,
     display: DisplayLocation,
-    todos_len: u16,
+    // todos_len: u16,
     todos_total_len: u16,
     todos: Vec<HashMap<String, String>>,
+    todo_item: HashMap<String, String>,
+    title: String,
+    description: String,
 }
 
 impl Display {
     pub fn new(
         maxc: u16,
         maxl: u16,
-        todos_len: &mut u16,
+        // todos_len: &mut u16,
         todos_total_len: &mut u16,
         todos: Vec<HashMap<String, String>>,
     ) -> Display {
@@ -37,15 +49,18 @@ impl Display {
             maxl,
             current_todo: 2,
             display: DisplayLocation::Main,
-            todos_len: *todos_len,
+            // todos_len: *todos_len,
             todos_total_len: *todos_total_len,
             todos,
+            todo_item: HashMap::new(),
+            title: String::new(),
+            description: String::new(),
         }
     }
 
-    pub fn display_holder(&mut self, stdout: &mut Stdout) -> Result<(), std::io::Error> {
+    pub fn display_holder(&mut self, stdout: &mut Stdout, database: &mut Database) -> Result<(), std::io::Error> {
         while self.display != DisplayLocation::Quit {
-            handle_pressed_keys(self);
+            handle_pressed_keys(self, database);
             // match self.display {
             //     DisplayLocation::Todo => {}
             //     _ => {}
@@ -54,4 +69,75 @@ impl Display {
         }
         Ok(())
     }
+}
+
+enum Field {
+    Title,
+    Description,
+}
+
+pub fn read_text_boxes(display: &mut Display) -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let mut title = String::new();
+    let mut description = String::new();
+    let mut current_field = Field::Title;
+
+    stdout.execute(cursor::MoveTo(0, 0))?;
+    stdout.flush()?;
+    stdout.execute(Show)?;
+
+    loop {
+        if event::poll(Duration::from_millis(100))? {
+            match event::read()? {
+                Event::Key(KeyEvent { code, .. }) => {
+                    match code {
+                        KeyCode::Esc => {
+                            // Exit the loop
+                            break;
+                        }
+                        KeyCode::Tab => {
+                            // Switch between text boxes
+                            *&mut current_field = if let Field::Title = current_field {
+                                Field::Description
+                            } else {
+                                Field::Title
+                            };
+                            update_screen(display, title.clone(), description.clone());
+                        }
+                        // KeyCode::Enter => {
+                        //     // Handle Enter key (optional)
+                        // }
+                        KeyCode::Backspace => {
+                            // Handle Backspace key
+                            if let Field::Title = current_field {
+                                title.pop();
+                            } else {
+                                description.pop();
+                            }
+                            update_screen(display, title.clone(), description.clone());
+                        }
+                        KeyCode::Char(c) => {
+                            // Handle character input
+                            if let Field::Title = current_field {
+                                title.push(c);
+                            } else {
+                                description.push(c);
+                            }
+                            update_screen(display, title.clone(), description.clone());
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+    stdout.execute(Hide)?;
+    Ok(())
+}
+
+fn update_screen(display: &mut Display, title: String, description: String) {
+    display.title = title.clone();
+    display.description = description.clone();
+    let _ = draw(display, components::add_todo_component);
 }
