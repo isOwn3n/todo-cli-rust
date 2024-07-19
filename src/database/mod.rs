e846171ba9073a991197b5de9a3fb86079b85e6d
ownen <mr.joker.mr.joke@gmail.com>
2024-07-20 03:20:10 +0330

    create a function to init db

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
+// use repository::add_new_todo;
 use sqlite::{Connection, Result};
 
 pub mod queries;
+pub mod repository;
 
 pub struct Database {
     connection: Connection,
@@ -16,27 +18,31 @@ impl Database {
     }
 }
 
-pub fn get_data(maxl: u16) -> Vec<HashMap<String, String>> {
-    let mut conn = Database::new("db.sqlite");
-    queries::get_all_todos(&mut conn.connection, (maxl / 2) - 1, 0).unwrap()
+pub fn init_database() -> Result<Database> {
+    Ok(Database::new("db.sqlite"))
 }
 
-pub fn get_count() -> Result<u16> {
-    let mut conn = Database::new("db.sqlite");
-    Ok(queries::get_count(&mut conn.connection)?)
+pub fn create_table(conn: &mut Database) -> Result<()> {
+    repository::create_todos_table(&mut conn.connection);
+    Ok(())
 }
 
-// pub fn get_count<'a>(conn: &'a mut Connection) -> Result<Statement<'a>> {
-//     let mut conn = Database::new("db.sqlite");
-//     let mut data = queries::Todo::count(conn)?;
-//     if let Ok(State::Row) = data.next() {
-//         Ok(data
-//             .read::<String, _>("count")
-//             .unwrap()
-//             .as_str()
-//             .parse()
-//             .unwrap())
-//     } else {
-//         Ok(0)
-//     }
+pub fn get_data(conn: &mut Database, maxl: u16) -> Vec<HashMap<String, String>> {
+    repository::get_all_todos(&mut conn.connection, (maxl / 2) - 1, 0).unwrap()
+}
+
+pub fn get_count(conn: &mut Database) -> Result<u16> {
+    Ok(repository::get_count(&mut conn.connection)?)
+}
+
+pub fn done(conn: &mut Database, id: u32) -> Result<()> {
+    repository::done_todo(&mut conn.connection, id)
+}
+
+pub fn get_one_todo(conn: &mut Database, id: u32) -> HashMap<String, String> {
+    repository::get_todo(&mut conn.connection, id).unwrap()
+}
+
+// pub fn new_todo(conn: &mut Database,title: String, description: String) -> Result<()> {
+//     add_new_todo(&mut conn.connection, title.as_str(), description.as_str())
 // }
